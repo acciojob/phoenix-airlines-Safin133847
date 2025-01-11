2bcd92a22cfd5af0a484915cd8d48802b23955cf
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-11 12:15:36 +0530

    Update flightReduser.js

--- src/reducers/flightReduser.js
+++ src/reducers/flightReduser.js
@@ -1,1 +1,20 @@
+import { SET_SEARCH_CRITERIA, SET_BOOKING_DETAILS } from '../actions/flightActions';
+
+const initialState = {
+  searchCriteria: {},
+  bookingDetails: {}
+};
+
+const flightReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_SEARCH_CRITERIA:
+      return { ...state, searchCriteria: action.payload };
+    case SET_BOOKING_DETAILS:
+      return { ...state, bookingDetails: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default flightReducer;
 
