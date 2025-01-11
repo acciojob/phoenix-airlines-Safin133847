640f9587845a4b25becce9beeccd18cdd6aad57a
Sk Safin uddin <sksafinuddin123@gmail.com>
2025-01-11 17:19:55 +0530

    submitting code-Sat Jan 11 2025 17:19:53 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import FlightSearchPage from './components/FlightSearchPage';
-import FlightBookingPage from './components/FlightBookingPage';
-import ConfirmationPage from './components/ConfirmationPage';
-import './styles/App.css';
+import LandingPage from './LandingPage';
+import FlightSearchPage from './FlightSearchPage';
+import FlightBookingPage from './FlightBookingPage';
+import ConfirmationPage from './ConfirmationPage';
+import '../styles/App.css';
 
 const App = () => {
   return (
