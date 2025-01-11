9c6fc2e550e7417825008085edb84026ae9beb2b
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-11 12:20:13 +0530

    Create ConfirmationPage.js

---
+++ src/components/ConfirmationPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const ConfirmationPage = () => {
+  const { searchCriteria, bookingDetails } = useSelector(state => state.flight);
+
+  return (
+    <div className="confirmation-page">
+      <h1>Booking Confirmation</h1>
+      <p>Your flight from {searchCriteria.source} to {searchCriteria.destination} on {searchCriteria.date} has been booked successfully.</p>
+      <p>Booking Details:</p>
+      <p>Name: {bookingDetails.name}</p>
+      <p>Email: {bookingDetails.email}</p>
+      <p>Contact: {bookingDetails.contact}</p>
+    </div>
+  );
+}
+
+export default ConfirmationPage;
