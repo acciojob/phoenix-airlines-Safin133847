2741ae58623d4b7cdbdbc15745420fe950d5dfb9
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-11 15:40:18 +0530

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
+    <div className="flight-booking">
       <h1>Welcome to Phoenix Airlines</h1>
       <Link to="/flight-search">Start Your Flight Search</Link>
     </div>
